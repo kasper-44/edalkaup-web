b3b5e4ef2920a72961b5499a7be03b83ae50c738
kasper-44 <kasper-44@users.noreply.github.com>
2026-04-19 20:43:11 +0000

    Update map to show Laugavegur 44, Reykjavík

--- src/app/hafa-samband/page.tsx
+++ src/app/hafa-samband/page.tsx
@@ -50,7 +50,7 @@ export default function HafaSambandPage() {
             {/* Map */}
             <div className="bg-white dark:bg-navy-800 rounded-2xl border border-black/5 dark:border-white/5 overflow-hidden">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1740.5!2d-21.9!3d64.14!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNjTCsDA4JzI0LjAiTiAyMcKwNTQnMDAuMCJX!5e0!3m2!1sis!2sis!4v1234567890"
+                src="https://maps.google.com/maps?q=Laugavegur+44,+101+Reykjavík,+Iceland&t=&z=16&ie=UTF8&iwloc=&output=embed"
                 width="100%"
                 height="250"
                 style={{ border: 0 }}
